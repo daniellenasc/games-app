8e9201369922cd798370d95cadd8e06993450a5e
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-03-31 17:41:27 +0200

    install axios

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import { Games } from "./components/Games/Games";
 import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Games />
     </div>
   );
 }
--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,4 +1,10 @@
+import { useState, useEffect } from "react";
+
 //create component Games as function - to render the API data
 export function Games() {
-    return ()
+  return (
+    <div>
+      <h1>Games</h1>
+    </div>
+  );
 }
