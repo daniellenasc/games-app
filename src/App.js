1f0a9b0166bbcf3179b30ae7cc9837e9e3173f73
Danielle Nascimento <danielle_nascimento_@hotmail.com>
2022-03-31 20:06:09 +0200

    create footer component, add footer style and add font-family in global style

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import { Footer } from "./components/Footer/Footer";
 import { Games } from "./components/Games/Games";
 import { Header } from "./components/Header/Header";
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <Header />
       <Games />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,9 @@
+export function Footer() {
+  return (
+    <footer className="footer">
+      <a href="https://www.linkedin.com/in/d-nascimento/">
+        Created by Danielle Nascimento
+      </a>
+    </footer>
+  );
+}
